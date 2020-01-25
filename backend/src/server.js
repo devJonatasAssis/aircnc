86f5037ea4c863b9cdae38d2d1feb5067251998c
Jonatas <jonatassilva9090@gmail.com>
2020-01-25 16:49:18 -0300

    Criando a tela de login da aplicação, salvando um usuário no banco de dados e acessando a api do nosso backend, instalada a dependência Cors para acessar de qualquer endereço

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-2zydt.mongodb.net/test?re
     useUnifiedTopology: true
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
