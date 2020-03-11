c3c2617be7793196915e28da85a9c4b928638573
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-03-11 13:53:45 -0300

    Fazendo a criacao de Spots pelo Insomnia, reserva

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 // Sessao
 routes.post('/sessions', SessionController.store);
 
 //Spots
-routes.post('/spots', SpotController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+
+//Dashboard
+routes.get('/dashboard', DashboardController.show);
+
+//Booking
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
