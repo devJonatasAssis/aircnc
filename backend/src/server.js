b2960e145f796652090fc20d9f971e2533b824b3
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-03-11 16:28:15 -0300

    Alterando spot com o virtual do Mongo, para acessar as imagens de dentro da url FILES

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -12,5 +13,6 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-e37qt.mongodb.net/test?re
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '../', 'uploads')));
 app.use(routes);
 app.listen(3333);
